2f8a5dc192a63b3415ba478a8aec69504076d15d
Максим Кудряшов <dunor@mail.ru>
2022-03-28 15:30:02 +0700

    change structur

--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Owner(models.Model):
-    name = models.CharField(max_length=50, verbose_name='имя')
-    cabinet = models.CharField(max_length=50, verbose_name='кабинет')
-
-    def __str__(self):
-        return self.name
